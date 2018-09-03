20cabd37a7c1f8110b3af3003677dd29f42ca815
SeyoungKim <ksy4961@gmail.com>
2018-09-03 11:57:43 +1000

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
