136eaf5ec5fd6502fc8e6d0132459fe3b8f7699b
SeyoungKim <ksy4961@gmail.com>
2018-09-24 21:05:38 +1000

    Added a recipe edit page and a router for it

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -15,7 +15,7 @@ export class RecipeEditComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params["id"];
       this.editMode = params["id"] != null;
-      // console.log(this.editMode); let us know which mode where we are
+      // console.log(this.editMode);  >>>> let us know which mode where we are
     });
   }
 }
